abf9f7b7ddb3cdbca2294f3866cdf08eb227d188
Jose <jmejiamu@gmail.com>
2021-09-26 00:28:56 -0700

    setup

---
+++ src/methodguide/Main.java
@@ -0,0 +1,17 @@
+
+package methodguide;
+
+/**
+ *
+ * @author JoseMejia62
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
