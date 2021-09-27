27e603b8e826f66c2e8b7df5f463c02a57069032
Jose <jmejiamu@gmail.com>
2021-09-27 01:12:56 -0700

    method overloading

--- src/methodguide/Main.java
+++ src/methodguide/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void methodName(){
         System.out.println("Code to executed when the method is call");
     }
-    
+    //Parameter
     public static void passingParams(String name){
         System.out.println("Hello, " + name + "!");
     }
@@ -18,7 +18,14 @@ public class Main {
         // Call a Method
         methodName();
         
+        // Argumenst
         passingParams("Twitter");
+        
+        int resultInt = MethodOverloading.sumValues(2, 7);
+        System.out.println("Int Result " + resultInt);
+        
+        double resultDouble = MethodOverloading.sumValues(4.9, 1.1);
+        System.out.println("Double Result " + resultDouble);
     }
     
 }
---
+++ src/methodguide/MethodOverloading.java
@@ -0,0 +1,16 @@
+
+package methodguide;
+
+/**
+ *
+ * @author JoseMejia62
+ */
+public class MethodOverloading {
+    public static int sumValues(int num1 , int num2){
+        return num1 + num2;
+    }
+    
+    public static double sumValues( double num1,double num2){
+        return num1 + num2;
+    }
+}
